99e59a76ad8b8a4fdde5a5eb20533e53a5603eee
AdityaSatyaWibawa <inyomanradityasatyawibawa@gmail.com>
2024-08-30 21:16:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Image Preview for About Section
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function () {
+        const output = document.getElementById('imagePreview');
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
